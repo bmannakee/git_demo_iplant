786c39f3ee82c6391f3dcf8c19be5e304de3b3fc
Brian Mannakee <bmannakee@gmail.com>
2015-02-22 15:39:19 -0700

    push everything

--- ggplot_learning.R
+++ ggplot_learning.R
@@ -11,12 +11,13 @@ library(reshape2)
 library(MASS)
 
 myplot <- ggplot(data=iris, aes(x=Sepal.Length,y=Sepal.Width,colour=Species)) 
-myplot <- myplot + geom_point(aes(shape=Species),size=3)
-#print(myplot)
+myplot <- myplot + geom_point(aes(shape=Species),size=3) + facet_grid(Species ~ .)
+myplot <- myplot + scale_fill_brewer(palette='Set1')
+print(myplot)
 
 d2 <- diamonds[sample(1:dim(diamonds)[1],1000),]
 plt1 <- ggplot(d2,aes(x=carat,y=price,colour=color))
-plt1 <- plt1 + geom_point(size=3)
+plt1 <- plt1 + geom_point(size=3) 
 print(plt1)
 
 plt2 <- ggplot(birthwt, aes(factor(race),bwt)) + geom_boxplot()
